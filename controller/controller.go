5906991427ebdbc1a0ddce8f9469ad940fe49439
hnam161098@gmail.com <hnam161098@gmail.com>
2024-08-19 22:40:56 +0700

    add env

--- controller/controller.go
+++ controller/controller.go
@@ -2,10 +2,13 @@ package controller
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
+
+var env = os.Getenv("ENV_ARGS")
 
 type ControllerHandler struct {
 }
@@ -16,6 +19,6 @@ func NewControllerHandler() *ControllerHandler {
 
 func (c *ControllerHandler) SayHello(ctx *fiber.Ctx) error {
 	t := time.Now().Format("15:04:03 02/01/2006")
-	msg := fmt.Sprintf("Bây giờ là %v", t)
+	msg := fmt.Sprintf("Đây là môi trường: %s và bây giờ là %v", env, t)
 	return ctx.JSON(msg)
 }
